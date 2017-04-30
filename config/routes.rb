2baa32c9c92cd2a9e44130f4eae85185e2cbaecd
Bruno Saibro Silveira <djbrunosilveira@gmail.com>
2017-04-30 09:16:47 -0300

    Introduces creating rooms functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :sessions
+  resources :rooms
+
   root to: 'rooms#index'
 end
---
+++ spec/factories/rooms.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :room do
+    title "MyString"
+  end
+end
