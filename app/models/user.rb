b4c607fd7ca249fb411d15f3b671cd631b5abd3c
Bruno Saibro Silveira <djbrunosilveira@gmail.com>
2017-04-30 10:24:07 -0300

    Gardening and user migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
   has_many :rooms, dependent: :destroy
   has_many :messages, dependent: :destroy
 end
